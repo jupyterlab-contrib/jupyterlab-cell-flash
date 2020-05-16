94992efcdf9e58d2b15076b4f9dfd27bd4e25f09
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-05-16 13:55:26 +0200

    Add color and duration to the settings

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,30 @@ import {
 
 import { NotebookActions } from '@jupyterlab/notebook';
 
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
 /**
  * Initialization data for the jupyterlab-cell-flash extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-cell-flash',
+  id: 'jupyterlab-cell-flash:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  optional: [ISettingRegistry],
+  activate: async (app: JupyterFrontEnd, settingRegistry: ISettingRegistry) => {
+
+    if (settingRegistry) {
+      const setting = await settingRegistry.load(extension.id);
+      const root = document.documentElement;
+      const updateSettings = () => {
+        const color = setting.get('color').composite as string;
+        const duration = setting.get('duration').composite as number;
+        root.style.setProperty('--jp-cell-flash-color', color);
+        root.style.setProperty('--jp-cell-flash-duration', `${duration}s`);
+      };
+      updateSettings();
+      setting.changed.connect(updateSettings);
+    }
+
     NotebookActions.executed.connect((_, args) => {
       const { cell } = args;
       const element = cell.editor.host;
